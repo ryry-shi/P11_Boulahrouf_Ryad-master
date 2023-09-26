e788c700fd4212fb1c9d7f25f614813d85993c4e
ryad <yoruzuya84@hotmail.fr>
2023-09-26 13:43:09 +0200

    fix bug one showshummary

---
+++ test_index.py
@@ -0,0 +1,7 @@
+from fichier_client import client
+
+def test_index(client):
+    response = client.get("/")
+    assert "Please enter your secretary email to continue:" in response.get_data(
+        as_text=True
+    )
